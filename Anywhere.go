16e7f09d15da960001d32ffe69fd0ed7c8def43b
Myts2 <xinxin134679@foxmail.com>
2019-04-22 23:14:42 +0800

    Create Anywhere.go
    With client and server

---
+++ Anywhere.go
@@ -0,0 +1,360 @@
+/*
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Juan Batiz-Benet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This program demonstrate a simple chat application using p2p communication.
+ *
+ */
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"crypto/rand"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"strings"
+	"sync"
+
+	ds "github.com/ipfs/go-datastore"
+	dsync "github.com/ipfs/go-datastore/sync"
+	golog "github.com/ipfs/go-log"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-crypto"
+	"github.com/libp2p/go-libp2p-host"
+	"github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p-net"
+	"github.com/libp2p/go-libp2p-peerstore"
+	pstore "github.com/libp2p/go-libp2p-peerstore"
+	rhost "github.com/libp2p/go-libp2p/p2p/host/routed"
+	"github.com/multiformats/go-multiaddr"
+	ma "github.com/multiformats/go-multiaddr"
+	gologging "github.com/whyrusleeping/go-logging"
+	"log"
+	"os"
+)
+var peer_lists []string
+
+func convertPeers(peers []string) []pstore.PeerInfo {
+	pinfos := make([]pstore.PeerInfo, len(peers))
+	for i, peer := range peers {
+		maddr := ma.StringCast(peer)
+		p, err := pstore.InfoFromP2pAddr(maddr)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		pinfos[i] = *p
+	}
+	return pinfos
+}
+
+func readpeers() {
+	var current_locate string
+	current_locate,_ = os.Getwd()
+	println(current_locate+"/peers.txt")
+	inputFile, inputError := os.Open(current_locate+"/peers.txt")
+	if inputError != nil {
+		fmt.Printf("An error occurred on opening the inputfile\n")
+		return
+	}
+	defer inputFile.Close()
+	inputReader := bufio.NewReader(inputFile)
+	for {
+		inputString, readerError := inputReader.ReadString('\n')
+		//inputString, readerError := inputReader.ReadBytes('\n')
+		if readerError == io.EOF {
+			return
+		}
+		inputString = strings.TrimSpace(inputString)
+		peer_lists = append(peer_lists, inputString)
+	}
+}
+// This code is borrowed from the go-ipfs bootstrap process
+func bootstrapConnect(ctx context.Context, ph host.Host, peers []pstore.PeerInfo) error {
+	if len(peers) < 1 {
+		return errors.New("not enough bootstrap peers")
+	}
+
+	errs := make(chan error, len(peers))
+	var wg sync.WaitGroup
+	for _, p := range peers {
+
+		// performed asynchronously because when performed synchronously, if
+		// one `Connect` call hangs, subsequent calls are more likely to
+		// fail/abort due to an expiring context.
+		// Also, performed asynchronously for dial speed.
+
+		wg.Add(1)
+		go func(p pstore.PeerInfo) {
+			defer wg.Done()
+			defer log.Println(ctx, "bootstrapDial", ph.ID(), p.ID)
+			log.Printf("%s bootstrapping to %s", ph.ID(), p.ID)
+
+			ph.Peerstore().AddAddrs(p.ID, p.Addrs, pstore.PermanentAddrTTL)
+			if err := ph.Connect(ctx, p); err != nil {
+				log.Println(ctx, "bootstrapDialFailed", p.ID)
+				log.Printf("failed to bootstrap with %v: %s", p.ID, err)
+				errs <- err
+				return
+			}
+			log.Println(ctx, "bootstrapDialSuccess", p.ID)
+			log.Printf("bootstrapped with %v", p.ID)
+		}(p)
+	}
+	wg.Wait()
+
+	// our failure condition is when no connection attempt succeeded.
+	// So drain the errs channel, counting the results.
+	close(errs)
+	count := 0
+	var err error
+	for err = range errs {
+		if err != nil {
+			count++
+		}
+	}
+	if count == len(peers) {
+		return fmt.Errorf("failed to bootstrap. %s", err)
+	}
+	return nil
+}
+
+func makeRoutedHost(listenPort int, bootstrapPeers []pstore.PeerInfo) (host.Host, error) {
+
+	// If the seed is zero, use real cryptographic randomness. Otherwise, use a
+	// deterministic randomness source to make generated keys stay the same
+	// across multiple runs
+	var r io.Reader
+
+		r = rand.Reader
+
+
+	// Generate a key pair for this host. We will use it at least
+	// to obtain a valid host ID.
+	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+	if err != nil {
+		return nil, err
+	}
+	opts := []libp2p.Option{
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", listenPort)),
+		libp2p.Identity(priv),
+		libp2p.DefaultTransports,
+		libp2p.DefaultMuxers,
+		libp2p.DefaultSecurity,
+		libp2p.NATPortMap(),
+	}
+
+	ctx := context.Background()
+
+	basicHost, err := libp2p.New(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct a datastore (needed by the DHT). This is just a simple, in-memory thread-safe datastore.
+	dstore := dsync.MutexWrap(ds.NewMapDatastore())
+
+	// Make the DHT
+	dht := dht.NewDHT(ctx, basicHost, dstore)
+
+	// Make the routed host
+	routedHost := rhost.Wrap(basicHost, dht)
+
+	// connect to the chosen ipfs nodes
+	err = bootstrapConnect(ctx, routedHost, bootstrapPeers)
+	if err != nil {
+		return nil, err
+	}
+
+	// Bootstrap the host
+	err = dht.Bootstrap(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build host multiaddress
+	hostAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ipfs/%s", routedHost.ID().Pretty()))
+
+	// Now we can build a full multiaddress to reach this host
+	// by encapsulating both addresses:
+	// addr := routedHost.Addrs()[0]
+	addrs := routedHost.Addrs()
+	log.Println("I can be reached at:")
+	for _, addr := range addrs {
+		log.Println(addr.Encapsulate(hostAddr))
+	}
+
+	log.Printf("Now run \"./Anywhere-go -l %d -d /ip4/127.0.0.1/tcp/%v/p2p/%s\" on a different terminal\n", listenPort+1,listenPort, routedHost.ID().Pretty())
+
+	return routedHost, nil
+}
+
+func handleStream(s net.Stream) {
+	log.Println("Got a new stream!")
+	/**********************************************************************/
+	// Create a buffer stream for non blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+	go readData(rw)
+	go writeData(rw)
+	/**********************************************************************/
+	// stream 's' will stay open until you close it (or the other side closes it).
+}
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, _ := rw.ReadString('\n')
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+
+		if err != nil {
+			panic(err)
+		}
+
+		rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		rw.Flush()
+	}
+
+}
+
+func main() {
+	golog.SetAllLoggers(gologging.INFO) // Change to DEBUG for extra info
+
+	// Parse options from the command line
+	listenF := flag.Int("l", 0, "wait for incoming connections")
+	target := flag.String("d", "", "target peer to dht")
+
+	flag.Parse()
+
+	if *listenF == 0 {
+		log.Fatal("Please provide a port to bind on with -l")
+	}
+
+	// Make a host that listens on the given multiaddress
+	var bootstrapPeers []pstore.PeerInfo
+
+	if *target != "" {
+		peer_lists = append(peer_lists,*target)
+		}
+	readpeers()
+	println("ok")
+	bootstrapPeers = convertPeers(peer_lists)
+
+	// 0.0.0.0 will listen on any interface device.
+
+	// libp2p.New constructs a new libp2p Host.
+	// Other options can be added here.
+	host, err := makeRoutedHost(*listenF, bootstrapPeers)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if *target == "" {
+		// Set a function as stream handler.
+		// This function is called when a peer connects, and starts a stream with this protocol.
+		// Only applies on the receiving side.
+		host.SetStreamHandler("/chat/1.0.0", handleStream)
+
+		// Let's get the actual TCP port from our listen multiaddr, in case we're using 0 (default; random available port).
+		var port string
+		for _, la := range host.Network().ListenAddresses() {
+			if p, err := la.ValueForProtocol(multiaddr.P_TCP); err == nil {
+				port = p
+				break
+			}
+		}
+
+		if port == "" {
+			panic("was not able to find actual local port")
+		}
+
+
+		fmt.Println("You can replace 127.0.0.1 with public IP as well.")
+		fmt.Printf("\nWaiting for incoming connection\n\n")
+
+		// Hang forever
+		<-make(chan struct{})
+	} else {
+		fmt.Println("This node's multiaddresses:")
+		for _, la := range host.Addrs() {
+			fmt.Printf(" - %v\n", la)
+		}
+		fmt.Println()
+
+		// Turn the destination into a multiaddr.
+		maddr, err := multiaddr.NewMultiaddr(*target)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Extract the peer ID from the multiaddr.
+		info, err := peerstore.InfoFromP2pAddr(maddr)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Add the destination's peer multiaddress in the peerstore.
+		// This will be used during connection and stream creation by libp2p.
+		host.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
+
+		// Start a stream with the destination.
+		// Multiaddress of the destination peer is fetched from the peerstore using 'peerId'.
+
+		/**********************************************************************/
+		s, err := host.NewStream(context.Background(), info.ID, "/chat/1.0.0")
+		if err != nil {
+			panic(err)
+		}
+
+		// Create a buffered stream so that read and writes are non blocking.
+		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+		// Create a thread to read and write data.
+		go writeData(rw)
+		go readData(rw)
+
+		// Hang forever.
+		select {}
+		/**********************************************************************/
+	}
+}
