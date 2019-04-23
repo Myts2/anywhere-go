1168c23938ae75de1675a63c3e40054b06dd9e12
xice09 <liangxiao09@163.com>
2019-04-24 04:25:05 +0800

    finish

--- server/go_server.go
+++ server/go_server.go
@@ -4,9 +4,10 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	p "github.com/Myts2/anywhere-go/common/proto"
+	"github.com/golang/protobuf/proto"
 	"io"
 	"net"
-	"strings"
 )
 
 var m = make(map[string]net.Conn)
@@ -38,59 +39,108 @@ func dialHost(hostname string, comConn string) (err int) {
 //	stop <- true
 //	return
 //}
-func storeConn(dict map[string]net.Conn, conn net.Conn) string {
+func storeConn(conn net.Conn) string {
 	signByte := []byte(conn.RemoteAddr().String())
 	hash := md5.New()
 	hash.Write(signByte)
 	hostName := hex.EncodeToString(hash.Sum(nil))[:8]
-	dict[hostName] = conn
+	m[hostName] = conn
 	return hostName
 }
 
-func connHandler(c net.Conn) {
+func connHandler(c net.Conn, h chan string) {
 	if c == nil {
 		return
 	}
-
-	hostname := storeConn(m, c)
-	defer delete(m, hostname)
-	_, e := c.Write([]byte("HM" + hostname))
-	if e != nil {
-		return
-	}
+	defer c.Close()
+	var hostname string
 	for {
-		buf := make([]byte, 4096)
+		buf := make([]byte, 40960)
 		cnt, err := c.Read(buf)
-		if err != nil || cnt == 0 {
-			e = c.Close()
-			if e != nil {
-				return
-			}
+		if err != nil {
 			break
 		}
-		if string(buf[0:2]) == "OK" {
-			_, _ = io.Copy(m[string(buf[2:10])], c)
-			break
-		} else {
-			rawHostnameInput := strings.TrimSpace(string(buf[0:cnt]))
-			hostnameInputs := strings.Split(rawHostnameInput, " ")
-			result := dialHost(hostnameInputs[0], hostname)
-			if result != -1 {
-				break
-			} else {
-				_, e = c.Write([]byte("WA"))
-				if e != nil {
-					return
-				}
-				continue
+		pkg := &p.Pkg{}
+		err = proto.Unmarshal(buf[:cnt], pkg)
+		if err != nil {
+			continue
+		}
+		switch pkg.Type {
+		case p.Type_Hello:
+			hostname = <- h
+			pkgToSend := &p.Pkg{
+				From: "",
+				To:   hostname,
+				Type: p.Type_Hello,
+				Data: nil,
 			}
+			b, _ := pkgToSend.Marshal()
+			_, e := c.Write(b)
+			if e != nil {
+				break
+			}
+		case p.Type_OK ,p.Type_PassWay, p.Type_ConnectClosed, p.Type_CantConnectSocks:
+			if co, ok := m[pkg.To]; ok {
+				_, err := co.Write(buf[:cnt])
+				if err != nil {
+					pkgToSend := &p.Pkg{
+						From: pkg.To,
+						To:   hostname,
+						Type: p.Type_ConnectClosed,
+						Data: nil,
+					}
+					b, _ := pkgToSend.Marshal()
+					_, err = c.Write(b)
+					if err != nil {
+						break
+					}
+				}
+			}
+			continue
+		case p.Type_WantToConnection:
+			if co, ok := m[pkg.To]; ok {
+				pkgToSend := &p.Pkg{
+					From: hostname,
+					To:   pkg.To,
+					Type: p.Type_ConnectionFrom,
+					Data: nil,
+				}
+				b, _ := pkgToSend.Marshal()
+				_, err = co.Write(b)
+				if err != nil {
+					pkgToSend := &p.Pkg{
+						From: pkg.To,
+						To:   hostname,
+						Type: p.Type_ConnectClosed,
+						Data: nil,
+					}
+					b, _ := pkgToSend.Marshal()
+					_, err = c.Write(b)
+					if err != nil {
+						break
+					}
+				}
+			}else {
+				pkgToSend := &p.Pkg{
+					From: pkg.To,
+					To:   hostname,
+					Type: p.Type_CantFind,
+					Data: nil,
+				}
+				b, _ := pkgToSend.Marshal()
+				_, err = c.Write(b)
+				if err != nil {
+					break
+				}
+			}
+			continue
+		default:
+			continue
 		}
 	}
 	fmt.Printf("Connection from %v closed. \n", c.RemoteAddr())
-	e = c.Close()
-	if e != nil {
-		return
-	}
+	delete(m, hostname)
+	return
 }
 
 func main() {
@@ -100,11 +150,13 @@ func main() {
 	}
 	fmt.Println("Server Started ...")
 	for {
+		h := make(chan string)
 		conn, err := server.Accept()
 		if err != nil {
 			fmt.Printf("Fail to connect, %s\n", err)
 			break
 		}
-		go connHandler(conn)
+		go connHandler(conn, h)
+		go func() {h <- storeConn(conn);close(h)}()
 	}
 }
